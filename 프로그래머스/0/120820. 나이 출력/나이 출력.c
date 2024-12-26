121dd0822f58ee3333a1c499f69311875a1c830c
Xotepsin <167324082+Xotepsin@users.noreply.github.com>
2024-12-26 10:30:56 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 4.39 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+    int answer;
+    if(age>0 && age<=120){
+        answer = 2022-age+1;
+    }
+
+    
+    return answer;
+}
