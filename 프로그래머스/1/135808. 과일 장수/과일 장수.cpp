05fdd55294580a1478e21ff80074a759570acb60
Xotepsin <167324082+Xotepsin@users.noreply.github.com>
2025-01-10 11:03:49 +0900

    [level 1] Title: 과일 장수, Time: 30.74 ms, Memory: 34.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+
+    sort(score.rbegin(), score.rend());
+
+
+    for (int i = 0; i + m <= score.size(); i += m) {
+
+        answer += score[i + m - 1] * m;
+    }
+
+    return answer;
+}
