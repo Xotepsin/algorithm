92ea53557c187e73ac357351320e633a45c0ebfa
Xotepsin <167324082+Xotepsin@users.noreply.github.com>
2025-01-31 09:16:34 +0900

    Update 문자열 나누기.cpp

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -4,15 +4,14 @@
 using namespace std;
 
 int solution(string s) {
-    int answer = 0;  // 분해된 문자열 개수
-    int i = 0;      // 문자열의 현재 위치
+    int answer = 0;
+    int i = 0;
 
     while (i < s.size()) {
         char x = s[i];
         int countX = 0;
         int countOther = 0;
 
-        // 분해 가능한 문자열 찾기
         while (i < s.size()) {
             if (s[i] == x) {
                 countX++;
